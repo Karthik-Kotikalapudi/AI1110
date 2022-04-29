e1f74252b82e9415f5cbed70cf48cb15eb1da35b
Karthik-Kotikalapudi <102539621+Karthik-Kotikalapudi@users.noreply.github.com>
2022-04-29 20:46:37 +0530

    Add files via upload

---
+++ Assignment-5/codes/code.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# array of all possible even numbers in a die
+arr = np.arange(1,7,2)
+
+#checking if 4 is in the above array
+if 4 in arr: 
+    print("Not mutually exclusive")
+else:
+    print("Mutually exclusive")
